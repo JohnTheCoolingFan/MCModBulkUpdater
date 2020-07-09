8924d15ce30908fd53fa1b5a91c79777849bdd63
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-07-09 21:48:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# MC bulk mods downloader
+-------------------------
+A tool for bulk downloading a list of mods. Supports curseforge and direct file links.
+Required packages: `requests`, `glob`, `hashlib`, `os`, `json`, `cloudscrapper`, `bs4`
+Mod list example included in repo.
+P.S. Example is a mod list for my modpack TBN3 (link here)
---
+++ automodlist.py
@@ -0,0 +1,24 @@
+import glob
+import json
+import hashlib
+
+files = glob.glob('*')
+files.remove('automodlist.py')
+
+with open('modlistdownload.json', 'r') as mlfile:
+    mdl = json.load(mlfile)
+    for file in files:
+        if file not in [mentry['filename'] for mentry in mdl]:
+            link = input('Download link for {}: '.format(file)).rstrip()
+            md5hash = hashlib.md5(open(file, 'rb').read()).hexdigest()
+            mglob = input('Glob for {}: '.format(file)).rstrip()
+            mentry = dict(link=link, filename=file, md5hash=md5hash, glob=mglob)
+            mdl.append(mentry)
+        else:
+            print('{} already in mod list. Skipping.'.format(file))
+    print('Finished gathering info, writing changes to file.')
+    mlfile.close()
+
+with open('modlistdownload.json', 'w') as mlfile:
+    json.dump(mdl, mlfile)
+    mlfile.close()
