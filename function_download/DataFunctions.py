6ed9dc567ecda7d93323b5bf573c124dc34e864b
Taruu <markov2002@bk.ru>
2020-07-12 00:40:10 +0300

    delite glob

--- function_download/DataFunctions.py
+++ function_download/DataFunctions.py
@@ -1,27 +1,51 @@
 import  requests
-
+from git_clone import git_clone
+import json
+from git import Repo
 servers = "https://raw.githubusercontent.com/Taruu/TANDOTS/master/server.json"
 
+#TODO logger
 class GetConfig:
-    def __init__(self):
-        pass
+    def __init__(self,Settings):
+        self.Settings = Settings
+        temp_result = requests.get(servers)
+        if temp_result.status_code == 200:
+            serverList = temp_result.json()
 
-    def Setting(self,Settings):
-        listConf = Settings.allKeys()
-        if not("oldservers" in listConf):
-            temp_result = requests.get(servers)
-            if temp_result.status_code == 200:
-                Settings.setValue("oldservers",temp_result.json())
+        listConf = self.Settings.allKeys()
+        for serverLocal in listConf:
+            if not(serverLocal in serverList):
+                self.Settings.remove(serverLocal)
+        for serverWeb in serverList:
+            if not(serverWeb in listConf):
+                self.Settings.setValue(serverWeb,{"name":serverWeb,"git":serverList[serverWeb],"path":None,"modList":None})
             else:
-                return "Not servers config" #TODO init this error
-        for server in Settings.value("oldservers"):
-            if not(server in listConf):
-                temp_result = requests.get(Settings.value("oldservers")[server]).json()
-                if temp_result.status_code == 200:
-                    Settings.setValue(server,temp_result.json())
-                else:
-                    return "Not config server!"
+                if serverList[serverWeb] != self.Settings.value(serverWeb)["git"]:
+                    tempServerSettings = self.Settings.value(serverWeb)
+                    tempServerSettings["git"] = serverList[serverWeb]
+                    self.Settings.setValue(serverWeb,tempServerSettings)
 
+
+    def CloneGit(self,url,path,progressBar): #TODO progressBar add and etc...
+        allZip = requests.get("url",steam=True)
+        with open(allZip, 'wb') as mod_file:
+            for chunk in allZip.iter_content(chunk_size=1024):
+                mod_file.write(chunk)
+                mod_file.flush()
+            mod_file.close()
+
+
+
+    def DownloadMods(self,serverName,path):
+
+        with open("modlistdownload.json") as modlist_file:
+            newModList = json.load(modlist_file)
+        listConf = self.Settings.allKeys()
+        if not (serverName in listConf):
+            self.Settings.setValue(serverName,{"path": path,"ModList": newModList})
+        else:
+            dataServerConfig = self.Settings.value(serverName)
+            oldModList = serverName["ModList"]
 
 
     def ConfigMods(self):
--- installer_mods.py
+++ installer_mods.py
@@ -1,12 +1,16 @@
 import sys
+import os
 import PyQt5
 from PyQt5 import QtGui,QtCore
 from PyQt5.QtGui import QTextCursor
 from PyQt5.QtCore import QUrl
 from PyQt5 import QtWidgets,uic
+
 from PyQt5.QtCore import QSize, QCoreApplication, QSettings,Qt,QThread
+from PyQt5.QtWidgets import QMessageBox
 from function_download import DataFunctions,ModsDownload
 import sys
+from pathlib import Path,PurePath
 from datetime import datetime
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
@@ -15,19 +19,15 @@ class Ui(QtWidgets.QMainWindow):
         met = QtGui.QFontMetrics(QtGui.QFont())
 
 
-        settings = QSettings('dataConfig', 'Tandots')
-        settings.clear()
-        print(settings.allKeys())
+        self.settings = QSettings('dataConfig', 'Tandots')
+        print()
 
         #custom function import here
-        self.GetConfig = DataFunctions.GetConfig()
-        self.GetConfig.Setting(settings)
+        self.GetConfig = DataFunctions.GetConfig(self.settings)
+        #self.settings.clear()
 
 
-        settings.setValue('list_value', [1, 2, 3])
 
-        list_value = settings.value('list_value', type=int)
-        print(list_value)
 
         #Take all elements
         self.comboBoxServer = self.findChild(QtWidgets.QComboBox,"comboBoxServer") #box on now server
@@ -38,14 +38,79 @@ class Ui(QtWidgets.QMainWindow):
         self.progressBar = self.findChild(QtWidgets.QProgressBar,"progressBar") #download_mod progressBar
 
         self.labelCountMods = self.findChild(QtWidgets.QLabel,"labelCountMods") #status / mods count label
+        self.labelPathInfo = self.findChild(QtWidgets.QLabel,"labelPathInfo") #status path
+
+        self.comboBoxServer.currentTextChanged.connect(self.takeServer)
+        self.pathMinecraft.editingFinished.connect(self.saveNowPath)
+        self.pushButtonTakePath.pressed.connect(self.takePath)
+        self.pushButtonDownload.pressed.connect(self.downloadAll)
+
+        for item in self.settings.allKeys():
+            self.comboBoxServer.addItem(item.upper())
 
         #init event functionality
 
-
+        self.nowServer = self.settings.value(self.comboBoxServer.currentText().lower())
+        self.check_path()
+        self.path = None
         self.show()
 
 
+    def check_path(self):
+        path = self.pathMinecraft.text()
+        if os.path.exists(path) and "minecraft" in path:
+            if os.path.exists(path+"/mods"):
+                self.saveNowPath()
+                self.path = path
+                self.labelPathInfo.setText("Path is OK!")
+            else:
+                self.labelPathInfo.setText("Not path mods!")
+                self.path = None
+                buttonReply = QMessageBox.question(self, 'Не нашли папку Mods...', "Не найдена папка mods создать? По адресу:\n{}".format(path), QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+                if buttonReply == QMessageBox.Yes:
+                    self.labelPathInfo.setText("Path is OK!")
+                    self.saveNowPath()
+                    self.path = path
+                    os.mkdir(path+"/mods")
+                else:
+                    self.labelPathInfo.setText("Not path mods!")
+                    self.path = None
+        else:
+            self.labelPathInfo.setText("Not correct path!")
+            self.path = None
 
+
+
+    def takeServer(self,text):
+        self.nowServer = self.settings.value(text.lower())
+        if self.nowServer["path"]:
+            self.pathMinecraft.setText(self.nowServer["path"])
+        else:
+            self.pathMinecraft.clear()
+
+    def saveNowPath(self):
+        print(self.nowServer,self.pathMinecraft.text())
+        self.nowServer["path"] = self.pathMinecraft.text()
+        self.settings.setValue(self.nowServer["name"],self.nowServer)
+
+    def takePath(self):
+        if os.path.exists(self.pathMinecraft.text()):
+            self.pathMinecraft.setText(
+                QtWidgets.QFileDialog.getExistingDirectory(self, 'Open mods directory', self.pathMinecraft.text()))
+        else:
+            self.pathMinecraft.setText(
+                QtWidgets.QFileDialog.getExistingDirectory(self, 'Open mods directory', str(Path.home())))
+
+
+        self.nowServer["path"] = self.pathMinecraft.text()
+        self.settings.setValue(self.nowServer["name"], self.nowServer)
+        self.check_path()
+
+    def downloadAll(self):
+        print(self.nowServer["git"],self.nowServer["path"])
+        self.GetConfig.CloneGit(self.nowServer["path"],self.nowServer["git"])
+        print("git has got")
+        pass
 
 
 
