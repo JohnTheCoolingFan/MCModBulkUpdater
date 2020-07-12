5617b9cf6c7421645e50688c7bca3d5825c5714d
Taruu <markov2002@bk.ru>
2020-07-12 13:52:24 +0300

    Need fix missing files

--- function_download/DataFunctions.py
+++ function_download/DataFunctions.py
@@ -1,13 +1,16 @@
 import  requests
-from git_clone import git_clone
 import json
-from git import Repo
+import zipfile
 servers = "https://raw.githubusercontent.com/Taruu/TANDOTS/master/server.json"
-
+import os
+import shutil
+import cloudscraper
+from bs4 import BeautifulSoup
 #TODO logger
 class GetConfig:
     def __init__(self,Settings):
         self.Settings = Settings
+        self.scraper = cloudscraper.create_scraper()
         temp_result = requests.get(servers)
         if temp_result.status_code == 200:
             serverList = temp_result.json()
@@ -26,27 +29,77 @@ class GetConfig:
                     self.Settings.setValue(serverWeb,tempServerSettings)
 
 
-    def CloneGit(self,url,path,progressBar): #TODO progressBar add and etc...
-        allZip = requests.get("url",steam=True)
-        with open(allZip, 'wb') as mod_file:
-            for chunk in allZip.iter_content(chunk_size=1024):
-                mod_file.write(chunk)
-                mod_file.flush()
-            mod_file.close()
+    def CloneGit(self, url, path_zip, progressBar): #TODO progressBar add and etc...
+        allZip = requests.get(url,stream=True)
+        content_length = allZip.headers.get("Content-Length")
+        while not(content_length):
+            allZip = requests.get(url, stream=True)
+            content_length = allZip.headers.get("Content-Length")
+        with open(path_zip, 'wb') as ZipClone:
+            progressBar.setMaximum(int(content_length)//1024)
+            for i,chunk in enumerate(allZip.iter_content(chunk_size=1024)):
+                progressBar.setValue(i)
+                ZipClone.write(chunk)
+                ZipClone.flush()
+            ZipClone.close()
+        work_path = os.path.dirname(path_zip)
+        with zipfile.ZipFile(path_zip, 'r') as zip_ref:
+            path_extacted = zip_ref.namelist()[0]
+            zip_ref.extractall(path=work_path)
+
+        for absolute,folder,items in os.walk(work_path+"/"+path_extacted):
+            absolute_path_need = absolute.replace("TBN3-master/", "")
+
+            for file in items:
+                if not(os.path.exists(absolute_path_need)):
+                    os.mkdir(absolute_path_need)
+                print(os.path.isdir(absolute_path_need),absolute_path_need)
+                if os.path.isdir(absolute_path_need):
+                    shutil.move(absolute +"/"+ file, absolute_path_need +"/"+ file)
+                else:
+                    shutil.move(absolute + file, absolute_path_need + file)
+        shutil.rmtree(work_path+"/"+path_extacted,)
+        os.remove(path_zip)
 
 
-
-    def DownloadMods(self,serverName,path):
-
-        with open("modlistdownload.json") as modlist_file:
+    def ListMods(self,path_work_folder):
+        with open(path_work_folder+"/modlistdownload.json") as modlist_file:
             newModList = json.load(modlist_file)
-        listConf = self.Settings.allKeys()
-        if not (serverName in listConf):
-            self.Settings.setValue(serverName,{"path": path,"ModList": newModList})
+        print(len(newModList))
+        return newModList
+
+    def DownloadMods(self,progressBar,path_work_folder,dataMod):
+        print("pathh",path_work_folder)
+        path_for_mods = path_work_folder + f"/mods/{dataMod['filename']}"
+        link = dataMod["link"]
+        mod = self.scraper.get(link, stream=True)
+        content_length = mod.headers.get("Content-Length")
+        print(mod.headers)
+        if link.startswith('https://www.curseforge.com'):
+            mod_screen = self.scraper.get(link)
+            soup = BeautifulSoup(mod_screen.text)
+            haslink = soup.findAll("p", {"class": "text-sm"})[0]
+            url = "https://www.curseforge.com" + haslink.findAll("a", href=True)[0].get('href')
+            mod_download = self.scraper.get(url, stream=True)
+            content_length = mod_download.headers.get("Content-Length")
+            while not (content_length):
+                mod_download = self.scraper.get(url, stream=True)
+                content_length = mod.headers.get("Content-Length")
         else:
-            dataServerConfig = self.Settings.value(serverName)
-            oldModList = serverName["ModList"]
+            mod_download = requests.get(link,stream=True)
+            content_length = mod.headers.get("Content-Length")
+            while not (content_length):
+                mod_download = requests.get(link,stream=True)
+                content_length = mod.headers.get("Content-Length")
 
 
-    def ConfigMods(self):
-        pass
+        with open(path_for_mods, 'wb') as ModFile:
+            progressBar.setMaximum(int(content_length) // 1024)
+            for i, chunk in enumerate(mod_download.iter_content(chunk_size=1024)):
+                progressBar.setValue(i)
+                ModFile.write(chunk)
+                ModFile.flush()
+            ModFile.close()
+        return True
+
+
