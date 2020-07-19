43d465df951c2d8ec79d463cd6890fc1a8614439
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-07-19 21:52:39 +0300

    Implemented args

--- automodlist.py
+++ automodlist.py
@@ -11,23 +11,26 @@ parser = argparse.ArgumentParser(description='Tool for easier creation of modlis
 parser.add_argument('FILE', required=True, help='File to use')
 parser.add_argument('--no-glob', action='store_const', const=False, default=True, help='Don\'t ask for glob; Don\'t add glob')
 
-files = glob.glob('*')
-files.remove('automodlist.py')
+args = vars(parser.parse_args())
 
-with open('testconfig/modlistdownload.json', 'r') as mlfile:
+files = glob.glob('*.jar')
+
+with open(args['FILE'], 'r') as mlfile:
     mdl = json.load(mlfile)
     for file in files:
         if file not in [mentry['filename'] for mentry in mdl]:
             link = input('Download link for {}: '.format(file)).rstrip()
             md5hash = hashlib.md5(open(file, 'rb').read()).hexdigest()
-            mglob = input('Glob for {}: '.format(file)).rstrip()
-            mentry = dict(link=link, filename=file, md5hash=md5hash, glob=mglob)
+            mentry = dict(link=link, filename=file, md5hash=md5hash)
+            if args['no-glob']:
+                mglob = input('Glob for {}: '.format(file)).rstrip()
+                mentry['glob'] = mglob
             mdl.append(mentry)
         else:
             print('{} already in mod list. Skipping.'.format(file))
     print('Finished gathering info, writing changes to file.')
     mlfile.close()
 
-with open('testconfig/modlistdownload.json', 'w') as mlfile:
+with open(args['FILE'], 'w') as mlfile:
     json.dump(mdl, mlfile, indent=4, sort_jeys=True)
     mlfile.close()
